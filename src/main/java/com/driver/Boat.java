21ae829c97d2ad12c72035430c074d3c97d479b1
Kiranchandu Saragadam <110031953+kiranchandusaragadam@users.noreply.github.com>
2023-10-17 20:50:57 +0530

    all classes done

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,18 @@
+package com.driver;
+
+public class Boat implements WaterVehicle{
+    private String name;
+    private int capacity;
+
+    public Boat(){
+
+    }
+
+    public String getVehicleName(){
+        return this.name;
+    }
+
+    public int getVehicleCapacity(){
+        return this.capacity;
+    }
+}
